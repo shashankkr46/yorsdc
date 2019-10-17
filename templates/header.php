d715d78effaad260e55dfa8684597bcce0813d7d
Shashank <shashankkr46@gmail.com>
2019-10-17 20:56:15 +0530

    Header converted to php title

--- templates/header.php
+++ templates/header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>Hello World</title>
+    <title><?php echo $page_title; ?></title>
 
     <!-- Required meta tags -->
     <meta charset="utf-8">
